1b9e74116b19fe7c7d7e663d0181a1c6d07635fa
gilang lubis <gilanglubis0@gmail.com>
2023-10-26 11:55:14 +0700

    gg

---
+++ app/src/main/java/com/example/curcolgan/FragEdit.java
@@ -0,0 +1,14 @@
+package com.example.curcolgan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frag_edit);
+    }
+}
--- app/src/main/java/com/example/curcolgan/FragProfile.java
+++ app/src/main/java/com/example/curcolgan/FragProfile.java
@@ -9,10 +9,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+
 public class FragProfile extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_profile, container,false);
+        return inflater.inflate(R.layout.frag_profile, container, false);
+    }
+
+    public void editprofil(View view) {
+        Intent intent = new Intent(getActivity(), FragPostingan.class);
+        startActivity(intent);
     }
 }
