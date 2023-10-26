aa99cd58b81ffb00ff0355260c81f22282c1d287
teachidien <gilanglubis0@gmail.com>
2023-10-26 15:53:54 +0700

    Tombol Edit Profil tersambung ke halaman edit profil

--- app/src/main/java/com/example/curcolgan/FragProfile.java
+++ app/src/main/java/com/example/curcolgan/FragProfile.java
@@ -4,22 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.content.Intent;
+import android.widget.Button; // Tambahkan baris ini untuk mengimpor kelas Button
+
 
 public class FragProfile extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_profile, container, false);
-    }
+        View view = inflater.inflate(R.layout.frag_profile, container, false);
 
-    public void editprofil(View view) {
-        Intent intent = new Intent(getActivity(), FragPostingan.class);
-        startActivity(intent);
+        // Menginisialisasi tombol "Edit Profil"
+        Button editProfileButton = view.findViewById(R.id.edit_profile_button);
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigasi ke FragEdit saat tombol "Edit Profil" ditekan
+                Intent intent = new Intent(getActivity(), FragEdit.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
